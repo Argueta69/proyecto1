059758dcb38bb5b785f094d872061dc5bbdc5291
Argueta01 <ajarguetab01@iesalbarregas.es>
2021-03-01 18:22:31 +0100

    Validando tercer commit

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,5 +5,6 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo java-git");
 		System.out.println("Hola mundo java-git2");
+		System.out.println("Hola mundo java-git3");
 		}
 }
